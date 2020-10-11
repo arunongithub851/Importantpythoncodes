6eb6e6d6c30a0c54ca51e0cdbc9af4f5b355924f
arunongithub851 <66999957+arunongithub851@users.noreply.github.com>
2020-10-11 18:01:35 +0530

    Create CodechefOctAddsqr.py

---
+++ CodechefOctAddsqr.py
@@ -0,0 +1,55 @@
+W, H, N, M = map(int, input().split())
+Vertical_Planes = list(map(int, input().split()))
+Horizontal_Planes = list(map(int, input().split()))
+horizontal_lengths = []
+Horizontal_Planes.sort()
+for i in range(len(Vertical_Planes)):
+    for j in range(i + 1, len(Vertical_Planes)):
+        side = abs(Vertical_Planes[i] - Vertical_Planes[j])  #Hint : - To shorten this loop try out adding next one to previous vals.
+        horizontal_lengths.append(side)
+#print(horizontal_lengths)
+horizontal_lengths = set(horizontal_lengths)
+horizontal_lengths = list(horizontal_lengths)
+vertical_possible_lengths = []
+for i in range(len(Horizontal_Planes)):
+    for j in range(i + 1, len(Horizontal_Planes)):
+        side = abs(Horizontal_Planes[i] - Horizontal_Planes[j])
+        vertical_possible_lengths.append(side)
+#print(vertical_possible_lengths)
+vertical_possible_lengths = set(vertical_possible_lengths)
+vertical_possible_lengths = list(vertical_possible_lengths)
+p = 0
+while True:
+    if vertical_possible_lengths[p] not in horizontal_lengths:
+        vertical_possible_lengths.remove(vertical_possible_lengths[p])
+    else:
+        p = p + 1
+    if p >= len(vertical_possible_lengths):
+        break
+#print(vertical_possible_lengths)
+if len(vertical_possible_lengths) == len(horizontal_lengths):
+    print(len(vertical_possible_lengths))
+else:
+    req_lengths = []
+    testcases = []
+    for i in horizontal_lengths:
+        if i not in vertical_possible_lengths:
+            req_lengths.append(i)
+    for j in req_lengths:
+        for i in Horizontal_Planes:
+            if i + j <= H:
+                testcases.append(i + j)
+            if i - j >= 0:
+                testcases.append(i - j)
+
+    count = 0
+    max_count = 0
+    testcases.sort()
+    testC = set(testcases)
+    testC = list(testC)
+    for k in testC:
+        count = testcases.count(k)
+        if count > max_count:
+            max_count = count
+
+    print(len(vertical_possible_lengths) + max_count)
